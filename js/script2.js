a3ade06d3143919ade4748a63d6dcd276afe45d6
rakeshkhaarol <rakeshkharol457@gmail.com>
2022-12-09 20:14:23 +0530

    script2 update

---
+++ js/script2.js
@@ -0,0 +1,23 @@
+
+const _news =[
+    {
+        nh:'‘Will be honest broker’: Sweden pledges hard push to EU-India free trade pact',
+        publish:"Published on Dec 09, 2022 08:00 PM IST",
+        np:`Swedish foreign trade minister Johan Forssell said Sweden will act as an “honest broker” and do its best to assist in the negotiations for the EU-India FTA. “This is also based on trust, you can’t have it my way or the highway”`
+    },
+] 
+
+
+//create expression
+let nex = "np";//stetment
+
+
+console.log(_news[0].nh)
+console.log(_news[0]['publish'])
+console.log(_news[0][nex])
+
+
+
+console.log("`/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\`")
+console.log('"/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\"')
+console.log('`/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\`')
